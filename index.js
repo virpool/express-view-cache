e44b6492da46bbd6b812b2b729fa29f5d842e8fb
Viktor Konovalov <kde3.5virpool@gmail.com>
2014-05-14 22:03:16 +0300

    Add ability to create/specify custom key

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ var adapterMemory = require('./lib/adapterMemory.js'),
 // Caching middleware for Express framework
 // details are here https://github.com/vodolaz095/express-view-cache
 
+function createCacheKeyDefault(request) {
+    return request.originalUrl;
+}
 
-module.exports=function(invalidateTimeInMilliseconds,parameters){
-    if(invalidateTimeInMilliseconds && /^\d+$/.test(invalidateTimeInMilliseconds)){
-        //it is ok
-    } else {
-        invalidateTimeInMilliseconds=60*1000; //1 minute
+module.exports = function (invalidateTimeInMilliseconds, parameters) {
+    if (!invalidateTimeInMilliseconds || isNaN(invalidateTimeInMilliseconds)) {
+        invalidateTimeInMilliseconds = 60*1000; //1 minute
     }
 
+    parameters = parameters || {};
+
     var cache;
-    if (parameters && parameters.driver) {
+    if (parameters.driver) {
         switch (parameters.driver) {
             case 'memjs':
                 cache = adapterMemJS;
@@ -22,36 +25,37 @@ module.exports=function(invalidateTimeInMilliseconds,parameters){
             case 'redis':
                 cache = adapterRedis;
                 break;
-            default :
+            default:
                 cache = adapterMemory;
         }
     } else {
         cache = adapterMemory;
     }
 
-    return function(request,response,next){
-        if(parameters && parameters.type){
+    return function (request, response, next) {
+        if (parameters.type) {
             response.type(parameters.type);
         }
         if (request.method == 'GET') {
-            cache.get(request.originalUrl,function(err,value){
-                if(value){
-                    //console.log('FRONT_CACHE HIT: GET '+request.originalUrl);
+            var targetKey = (parameters.createCacheKey || createCacheKeyDefault)(request);
+            cache.get(targetKey, function (err, value) {
+                if (value) {
+                    //console.log('FRONT_CACHE HIT: GET '+targetKey);
                     response.header('Cache-Control', "public, max-age="+Math.floor(invalidateTimeInMilliseconds/1000)+", must-revalidate");
                     response.send(value);
                     return true;
                 } else {
                     //http://stackoverflow.com/questions/13690335/node-js-express-simple-middleware-to-output-first-few-characters-of-response?rq=1
                     var end = response.end;
-                    response.end = function(chunk, encoding){
+                    response.end = function (chunk, encoding) {
                         response.end = end;
-                        response.on('finish',function(){
-                            cache.set(request.originalUrl,chunk,function(err,result){
-                                if(err) throw err;
-                                /*if(result){
-                                    console.log('FRONT_CACHE SAVED: GET '+request.originalUrl);
+                        response.on('finish', function () {
+                            cache.set(targetKey, chunk, function (err, result) {
+                                if (err) throw err;
+                                /*if (result) {
+                                    console.log('FRONT_CACHE SAVED: GET '+targetKey);
                                 } else {
-                                    console.log('FRONT_CACHE ERROR SAVING: GET '+request.originalUrl)
+                                    console.log('FRONT_CACHE ERROR SAVING: GET '+targetKey)
                                 }*/
                             },invalidateTimeInMilliseconds);
                         });
